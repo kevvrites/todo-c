90939e4219f57c206330700d42bf02c68fa32b0d
Kevin Liu <liukproductivity@gmail.com>
2024-01-17 15:24:29 -0500

    Working added tasks function using prepared statements

--- todo.c
+++ todo.c
@@ -22,8 +22,6 @@ void initialize_db()
 
     rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
         sqlite3_close(db);
         return;
     }
@@ -33,27 +31,47 @@ void initialize_db()
 
 void add_task(sqlite3 *db, const char *name,const char *due_date, const char *description)
 {
-    char *err_msg = 0;
+    sqlite3_stmt *stmt;
     int rc;
+    const char *sql;
 
-    char *sql_template = "INSERT INTO Tasks (Name, DueDate, Description) VALUES ('%q', '%q', '%q');";
-    char *sql = sqlite3_mprintf(sql_template, name, due_date, description)
-
-    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    sql = "INSERT INTO Tasks (Name, DueDate, Description) VALUES (?, ?, ?);";
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
+        fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+    
+    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, due_date, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 3, description, -1, SQLITE_TRANSIENT);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
     } else {
         printf("Task added successfully\n");
     }
 
-    sqlite3_free(sql);
 }
 
 int main()
 {
-    printf("Hello, world!\n");
+    sqlite3 *db;
+    int rc;
+
     initialize_db();
-    printf("Initialized database\n");
+
+    rc = sqlite3_open("todo.db", &db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error opening: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    add_task(db, "Test", "2024-01-20", "Sample test task");
+
+    sqlite3_close(db);
+
     return 0;
 }
